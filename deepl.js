f244ef570a0fc7f5d7f816ffc6c371cf565599c5
Richard <richardvu.work@gmail.com>
2021-07-05 15:53:47 +0700

    Update Deepl

--- deepl.js
+++ deepl.js
@@ -1,5 +1,5 @@
 module.exports = function(RED) {
-  function LowerCaseNode(config) {
+  function DeeplNode(config) {
       RED.nodes.createNode(this,config);
       var node = this;
       node.on('input', function(msg) {
